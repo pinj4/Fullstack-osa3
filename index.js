6384404c9d2750b905bf06e3a7bc39bbd3f4739b
Pinja <pinja.salomaki@gmail.com>
2024-01-24 17:47:28 +0200

    3.6 tehty

--- index.js
+++ index.js
@@ -6,60 +6,60 @@ app.use(express.json())
 
 let persons = [
       {
-        name: "Arto Hellas",
-        number: "040-123456",
+        name: 'Arto Hellas',
+        number: '040-123456',
         id: 1
       },
       {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
+        name: 'Ada Lovelace',
+        number: '39-44-5323523',
         id: 2
       },
       {
-        name: "Dan Abramov",
-        number: "12-43-234345",
+        name: 'Dan Abramov',
+        number: '12-43-234345',
         id: 3
       },
       {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
+        name: 'Mary Poppendieck',
+        number: '39-23-6423122',
         id: 4
       }
     ]
 
 app.get('/', (request, response) => {
-    console.log("hello world")
+    console.log('hello world')
     response.send('<h1>Hello World!</h1>')
 })
       
 app.get('/api/persons', (request, response) => {
-    console.log("get all")
+    console.log('get all')
     response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  console.log("get person")
+  console.log('get person')
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
   if (person) {
-      console.log("found")
+      console.log('found')
       response.json(person)
     } else {
-    console.log("not found")
+    console.log('not found')
       response.status(404).end()
     }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  console.log("delete ", id)
+  console.log('delete ', id)
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
 
 app.get('/api/info', (request, response) => {
-  console.log("info")
+  console.log('info')
   response.send(
     `Phonebook has info for ${persons.length} people
     <p>${new Date()}</p>`
@@ -72,20 +72,28 @@ const generateId = () => {
 }
 
 app.post('/api/persons', (request, response) => {
-  console.log("post new")
+  console.log('post new')
   const body = request.body
 
   if (!body.name) {
-    console.log("name missing")
+    console.log('name missing')
     return response.status(400).json({ 
       error: 'name missing' 
     })
   }
 
   if (!body.number) {
-    console.log("number missing")
+    console.log('number missing')
     return response.status(400).json({ 
       error: 'number missing' 
+    })
+  }
+
+  const persons_names = persons.map(person => person.name)
+  if (persons_names.includes(body.name)) {
+    console.log('name must be unique')
+    return response.status(400).json({ 
+      error: 'name must be unique' 
     })
   }
 
@@ -96,7 +104,7 @@ app.post('/api/persons', (request, response) => {
   }
 
   persons = persons.concat(person)
-  console.log("add ", person)
+  console.log('add ', person)
 
   response.json(person)
 })
