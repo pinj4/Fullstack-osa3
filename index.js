dd0f269c440a58e9454ed990a0eed95f08017e9a
Pinja <pinja.salomaki@gmail.com>
2024-01-31 08:49:26 +0200

    3.11

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
-
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', function (req, res) { 
   if(JSON.stringify(req.body) != '{}') {
