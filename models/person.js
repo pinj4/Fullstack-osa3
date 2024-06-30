f6595230b2f70f92cf3cab50ae3789926b3a9fa8
pinj4 <pinja.salomaki@gmail.com>
2024-06-30 13:57:23 +0300

    3.19 ja 3.20 tehty

--- models/person.js
+++ models/person.js
@@ -14,8 +14,22 @@ mongoose.connect(url)
   })
   
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: [5, 'name needs to be at least 5 characters long'],
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: [8, 'phone number needs to be at least 8 characters long'],
+    validate: {
+        validator: function(v) {
+            return (/\d{3}-\d{5}/.test(v) || /\d{2}-\d{6}/.test(v))
+        },
+        message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true,
+  },
   id: mongoose.ObjectId,
 })
 
